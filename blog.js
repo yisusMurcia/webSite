f6b3a27a221bd91a3961703e3a25a67decadeb06
Jesus Antonio Murcia <136138395+yisusMurcia@users.noreply.github.com>
2023-07-13 18:49:53 -0500

    13 Julio 2023
    Nuevo post

--- blog.js
+++ blog.js
@@ -4,7 +4,8 @@ let postArray=[
     ["Primeros pasos en HTML", "https://programmind.notion.site/Primeros-pasos-en-HTMl-b712e24ac56d447faf63f7b2131d0c8a?pvs=4"],
     ["Un poco de estilo", "https://programmind.notion.site/Un-poco-de-estilo-76537578429a49f089ff847ffa096e27?pvs=4"],
     ["Agregando cometarios", "https://programmind.notion.site/Comentarios-ec4c2c2062ef4229a38d39270ed191e2?pvs=4"],
-    ["Más que solo párrafos", "https://programmind.notion.site/HTML-es-m-s-que-p-rrafos-y-t-tulos-ac7ab7e1ed51448a941b521b6f40d016?pvs=4"]
+    ["Más que solo párrafos", "https://programmind.notion.site/HTML-es-m-s-que-p-rrafos-y-t-tulos-ac7ab7e1ed51448a941b521b6f40d016?pvs=4"],
+    ["Definiendo el tamaño", "https://programmind.notion.site/Definiendo-el-tama-o-5cf14fa72a2c42799a12a43db6e816ff?pvs=4"]
 ];
 let blogContent= document.getElementById("blogContent");
         for (let post of postArray.reverse()){
